539d5fbdd778be670799f025658d267dc4c49437
Raul Morais <raulmmorais@gmail.com>
2018-08-31 16:03:03 -0300

    inclusão de novos métodos, acho que consigo terminar hoje

--- js/functions.js
+++ js/functions.js
@@ -31,3 +31,24 @@ function handleFiles(e) {
         alert('the image is drawn');
     }
 }
+
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+  console.log("Nice")
+}else {
+  console.log('Sorry');
+}
+
+function handleFileSelect(evt) {
+  var files = evt.target.files; // FileList object
+
+  // files is a FileList of File objects. List some properties.
+  var output = [];
+  for (var i = 0, f; f = files[i]; i++) {
+    output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                f.size, ' bytes, last modified: ',
+                f.lastModifiedDate.toLocaleDateString(), '</li>');
+  }
+  document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+}
+
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
