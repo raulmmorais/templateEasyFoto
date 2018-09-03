82b1e17656c8db4b794447cfdedb5d6389183ebc
Raul Morais <raulmmorais@gmail.com>
2018-09-03 07:13:25 -0300

    melhorias

--- js/thunbs.js
+++ js/thunbs.js
@@ -10,11 +10,26 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
 function handleFileSelect(evt) {
   var files = evt.target.files; // FileList object
   // files is a FileList of File objects. List some properties.
-  var output = [];
+  var idImg = 0
   for (var i = 0, f; f = files[i]; i++) {
-    output.push('<li><strong><a href="#" onclick="handleFileToCrop(document.getElementById(\'files\').files['+ i +'])">', escape(f.name), '</a></strong> (', f.type || 'n/a', ') - ',
-                f.size, ' bytes, last modified: ',
-                f.lastModifiedDate.toLocaleDateString(), '</li>');
+    if (!f.type.match('image.*')) {
+        continue;
+    }
+    var reader = new FileReader()
+    reader.onload = (function(theFile){
+      return function(e){
+        var span = document.createElement('span')
+        span.innerHTML = ['<a href="#" onclick="handleFileToCrop(document.getElementById(\'files\').files['+ idImg +'])">'+
+        '<img class="thumb" src="', e.target.result,
+        '" title="', escape(theFile.name), '"/></a>'].join('')
+        document.getElementById('list').insertBefore(span, null)
+        idImg++
+      }
+    })(f)
+    reader.readAsDataURL(f);
+    //output.push('<li><strong><a href="#" onclick="handleFileToCrop(document.getElementById(\'files\').files['+ i +'])">', escape(f.name), '</a></strong> (', f.type || 'n/a', ') - ',
+    //            f.size, ' bytes, last modified: ',
+    //            f.lastModifiedDate.toLocaleDateString(), '</li>');
   }
-  document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+  //document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
 }
