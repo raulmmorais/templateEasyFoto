477a0499eb9f1ff16597062e4254af58b1c2d165
Raul Morais <raulmmorais@gmail.com>
2018-10-21 21:31:10 -0300

    Primeiro Commit

--- projeto/app/webapp/imageController.js
+++ projeto/app/webapp/imageController.js
@@ -9,6 +9,9 @@
         const vm = this;
         vm.title = "Escolha as imagens do seu computador"
         vm.result = [];
+        vm.images = [];
+        vm.index = 0;
+        vm.files = null;
 
         vm.getImages = function(){
           console.log("Metodo getImages()");
@@ -17,6 +20,7 @@
           for (var i = 0; i < files.length; i++) {
             vm.result.push({"name": files[i].name, "image": fileReader.readAsDataUrl(files[i], $scope)});
           }
+
           element.value = ""
         }
 
@@ -33,6 +37,71 @@
             file = new File([blob], src.name , blob)
             vm.handleFileToCrop(file, index)
           })
+        }
+
+        vm.test3 = function(){
+            console.log("método test3()")
+            vm.files = document.getElementById('files').files;
+            for (var i = 0; i < files.length; i++) {
+                vm.result.push({"name": files[i].name, "image": fileReader.readAsDataUrl(files[i], $scope)});
+            }
+            vm.reader(vm.files[vm.index])
+        }
+        vm.test4 = function(){
+            if(crop.isImageSet()){
+                var img = crop.getCroppedImage(600, 600);
+                //img.className = "imagePreview"
+                img.onload = (function () {
+                    console.log("add images");
+                    return vm.addCropedImage(img);
+                });
+            }
+        }
+        vm.reader = function(file){
+            var img = new Image();
+            var reader = new FileReader();
+            img.addEventListener("load", function(){
+                crop.setImage(img);
+                vm.test4();
+            }, false);
+            reader.onload = function(){
+                img.src = reader.result;
+            }
+            if(file){
+                reader.readAsDataURL(file)
+            }
+        }
+        vm.addCropedImage = function(img){
+            if(img){
+                console.log('add cropped image')
+                vm.index++
+                vm.images.push({"src": img.src});
+                vm.reader(vm.files[vm.index]);
+            }
+        }
+
+        vm.test = function(){
+            console.log("metodo test()")
+            const files = document.getElementById('files').files;
+            var reader = new FileReader();
+            var img = new Image();
+            img.addEventListener("load", function(){
+                crop.setImage(img);
+                vm.test2();
+            }, false);
+            reader.onload = function(){
+                img.src = reader.result;
+            }
+            if(files[0]){
+                reader.readAsDataURL(files[0])
+            }
+        }
+        vm.test2 = function(){
+            if(crop.isImageSet()){
+                var img = crop.getCroppedImage(600, 600);
+                img.className = "imagePreview"
+                img.onload = (function () { return previewLoaded(img); });
+            }
         }
 
         vm.handleFileToCrop = function(file, index){
