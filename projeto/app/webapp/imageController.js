033b184b40dd22862a817f7b4e6509b53a850039
Raul Morais <raulmmorais@gmail.com>
2018-10-24 22:40:05 -0300

    Correções.

--- projeto/app/webapp/imageController.js
+++ projeto/app/webapp/imageController.js
@@ -9,10 +9,10 @@
         const vm = this;
         vm.title = "Escolha as imagens do seu computador"
         vm.result = [];
-        vm.images = [];
         vm.index = 0;
-        vm.files = [];
         vm.atualIndex = 0;
+        vm.valueToPay = 0;
+        vm.qtdFrames = 0;
 
         vm.getImages = function(){
           console.log("Method getImages()");
@@ -49,30 +49,30 @@
           imgCropped.onload = (function(){
             vm.result[vm.index].croppedImage = imgCropped.src;
             vm.index++;
+            vm.qtdFrames++
             if(vm.result[vm.index]){
               vm.reader(vm.result[vm.index].file);
             }else {
               console.log("end of line");
               setTimeout(function(){ vm.showNames(); }, 1000);
+              //vm.qtdFrames = vm.result.length;
+              vm.valueToPay = vm.getValueToPay(vm.qtdFrames);
             }
           });
         }
         vm.onLoadImage = function(index){
           console.log("index: ", index);
         }
-        vm.test4 = function(){
-            if(crop.isImageSet()){
-                var img = crop.getCroppedImage(600, 600);
-                //img.className = "imagePreview"
-                img.onload = (function () {
-                    console.log("add images");
-                    return vm.addCropedImage(img);
-                });
-            }
+
+        vm.getValueToPay = function(qtd){
+            var value = qtd*25;
+            return value.toLocaleString("pt-BR", { minimumFractionDigits: 2 });
         }
 
         vm.deleteImage = function(index){
-          vm.result.splice(index, 1)
+          vm.result.splice(index, 1);
+          vm.qtdFrames--;
+          vm.valueToPay = vm.getValueToPay(vm.qtdFrames);
         }
 
         vm.cropImage = function(index){
@@ -84,14 +84,6 @@
             file = new File([blob], src.name , blob)
             vm.handleFileToCrop(file, index)
           })
-        }
-        vm.addCropedImage = function(img){
-            if(img){
-                console.log('add cropped image')
-                vm.index++
-                vm.images.push({"src": img.src});
-                vm.reader(vm.files[vm.index]);
-            }
         }
 
         vm.handleFileToCrop = function(file, index){
@@ -127,7 +119,13 @@
         }
 
         vm.showNames = function(){
-          vm.result.forEach(function(e, i){console.log(e.name)})
+          //vm.result.forEach(function(e, i){console.log(e.name)})
+          modal.disable();
+          console.log("Show names")
+        }
+
+        vm.pay = function(){
+            console.log("Time to pay.")
         }
     }
 })()
@@ -139,8 +137,10 @@ function previewLoaded(img) {
 var modalContainer = document.getElementById('ouibounce-modal')
 var modal = ouibounce(modalContainer, {
     aggressive: true,
-    timer: 0,
-    callback: function() { console.log('ouibounce fired!'); }
+    //timer: 0,
+    callback: function() {
+        console.log('ouibounce fired!');
+        modal.disable();
+    }
 });
 //I will use https://github.com/carlsednaoui/ouibounce to create a modal.
-//or https://github.com/likeastore/ngDialog#api
